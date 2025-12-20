a39ce7a6c6ca6137d9746b046769eeed2a404dcc
Mohammad <amirrahimi815@gmail.com>
2025-12-20 19:25:11 +0330

    new file

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+# # if satement
+# #if .... : print(salam)
+
+# number=1
+# number2=2
+# if number < number2 :
+#     print('True')
+# else:
+#     print('false')
+
+# number1=5
+# number2=9
+# if number1>number2 :
+#     print('true')
+# else:
+#     print('false')
+
